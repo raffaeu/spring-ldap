c721349370380cc0bd17678fe6f9b78d0338f95e
Raffaele Garofalo <raffaeu@gmail.com>
2018-11-11 15:10:11 +0100

    Final Commit including URL in Azure

---
+++ README.md
@@ -0,0 +1,29 @@
+# Spring Boot Security with LDAP
+
+This project is willing to demonstrate how to create an Authentication Mechanism
+using Spring and an LDAP repository.
+
+The Code can be build using `spring-boot` like this:
+
+```bash
+
+$ mvn spring-boot:run
+# served on http://locahost:8080
+
+```
+
+## Credentials
+
+Bob Hamilton <br />
+DEVELOPER <br />
+Username: `bob` <br />
+Password: `bobspassword` <br />
+
+Joe Smeth <br />
+MANAGER <br />
+Username: `joe` <br />
+Password: `joespassword` <br />
+
+## Deployment Demo
+
+A [demo is available](http://spring-ldap.azurewebsites.net/spring-ldap) on my Tomcat Azure subscription.
--- src/main/java/com/ubs/springldap/App.java
+++ src/main/java/com/ubs/springldap/App.java
@@ -2,6 +2,7 @@ package com.ubs.springldap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.ldap.repository.config.EnableLdapRepositories;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,7 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
